f0eda2c5b9ceb10d4bbca0952c6c7a704fc2b64b
pedrogiampietro <pedrogiampietro@hotmail.com>
2022-01-25 18:01:56 -0300

    add: create a main section, 4429

---
+++ frontend/src/components/MainSection/index.jsx
@@ -0,0 +1,13 @@
+import { withRouter } from 'react-router-dom';
+
+import studioBag from '../../assets/studio-bag.png';
+
+import './MainSection.styles.scss';
+
+export function MainSection({ history }) {
+  return (
+    <div>
+      <h1>MainSection</h1>
+    </div>
+  );
+}
